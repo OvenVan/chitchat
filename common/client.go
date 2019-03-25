c0fe073dac55dcdda526c1777370a3e825bcb5c3
Oven Van <yew_oven@outlook.com>
2019-03-25 20:57:34 +0800

    refactoring project structure.

---
+++ common/client.go
@@ -0,0 +1,168 @@
+package common
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"sync"
+	"time"
+)
+
+type ClientReadFunc func([]byte, ReadFuncer) error
+
+type Client struct {
+	ipaddr    string
+	dialDDL   time.Duration
+	delimiter byte
+
+	readfunc ClientReadFunc
+
+	eDer
+	eDerfunc eDinitfunc
+
+	conn       net.Conn
+	cancelfunc context.CancelFunc
+
+	//tempory used for readfunc
+	additional interface{}
+}
+
+type hConnerClient struct {
+	conn     net.Conn
+	d        byte
+	mu       *sync.Mutex //lock readfunc
+	readfunc ClientReadFunc
+	eD       *eDer
+}
+
+func NewClient(
+	ipremotesocket string,
+	delim byte, readfunc ClientReadFunc, additional interface{}) *Client {
+
+	c := &Client{
+		ipaddr:    ipremotesocket,
+		dialDDL:   0,
+		delimiter: delim,
+		readfunc:  readfunc,
+		eDer: eDer{
+			eU:     make(chan Errsocket),
+			closed: false,
+			mu:     new(sync.Mutex),
+			pmu:    nil,
+		},
+		additional: additional,
+	}
+	c.eDerfunc = errDiversion(&c.eDer)
+	return c
+}
+
+func (t *Client) Dial() error {
+	var (
+		err        error
+		ctx, cfunc = context.WithCancel(context.Background())
+		eC         = make(chan Errsocket)
+	)
+	t.cancelfunc = cfunc
+
+	if t.dialDDL == 0 {
+		t.conn, err = net.Dial("tcp", t.ipaddr)
+	} else {
+		t.conn, err = net.DialTimeout("tcp", t.ipaddr, t.dialDDL)
+	}
+	if err != nil {
+		return err
+	}
+	go t.eDerfunc(eC)
+	go handleConnClient(&hConnerClient{
+		conn:     t.conn,
+		d:        t.delimiter,
+		readfunc: t.readfunc,
+		mu:       t.mu,
+		eD:       &t.eDer,
+	}, eC, ctx, t)
+
+	return nil
+}
+
+func handleConnClient(h *hConnerClient, eC chan Errsocket, ctx context.Context, c *Client) {
+	fmt.Println("Start hCC:", h.conn.LocalAddr(), "->", h.conn.RemoteAddr())
+	defer fmt.Println("->hCC quit", h.conn.LocalAddr(), "->", h.conn.RemoteAddr())
+	strReqChan := make(chan []byte)
+	defer func() {
+		if !h.eD.closed {
+			h.mu.Lock()
+			h.eD.closed = true
+			close(h.eD.eU)
+			h.mu.Unlock()
+		}
+		err := h.conn.Close()
+		<-strReqChan
+		if err != nil {
+			h.mu.Lock()
+			eC <- Errsocket{err, h.conn.RemoteAddr().String()}
+		}
+		close(eC)
+	}()
+
+	go read(&reader{
+		conn:       h.conn,
+		d:          h.d,
+		mu:         h.mu,
+		strReqChan: strReqChan,
+	}, eC)
+
+	for {
+		select {
+		case <-ctx.Done(): //quit manually
+			return
+		case strReq, ok := <-strReqChan: //read a data slice successfully
+			if !ok {
+				return //EOF && d!=0
+			}
+			if h.readfunc != nil {
+				//h.mu.Lock()                  //s.mu
+				err := h.readfunc(strReq, c) //requires a lock from hL
+				//h.mu.Unlock()
+				if err != nil {
+					h.mu.Lock()
+					eC <- Errsocket{err, h.conn.RemoteAddr().String()}
+				}
+			}
+		}
+	}
+}
+
+func (t *Client) Close() {
+	t.mu.Lock()
+	t.closed = true
+	close(t.eU)
+	t.mu.Unlock()
+	t.cancelfunc()
+}
+
+func (t *Client) SetDeadLine(dDDL time.Duration) {
+	t.dialDDL = dDDL
+}
+
+func (t *Client) ErrChan() <-chan Errsocket {
+	return t.eU
+}
+
+func (t *Client) GetRemoteAddr() string {
+	if t.conn == nil {
+		return ""
+	}
+	return t.conn.RemoteAddr().String()
+}
+
+func (t *Client) GetConn() net.Conn {
+	return t.conn
+}
+
+func (t *Client) Addon() interface{} {
+	return t.additional
+}
+
+func (t *Client) Write(i interface{}) error {
+	return Write(t.conn, i, t.delimiter)
+}
---
+++ common/server.go
@@ -0,0 +1,239 @@
+package common
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net"
+	"sync"
+	"time"
+)
+
+type ServerReadFunc func([]byte, ReadFuncer) error
+
+type Server struct {
+	//unchangable data
+	ipaddr   string
+	readDDL  time.Duration
+	writeDDL time.Duration
+	//if delimiter is 0, then read until it's EOF
+	delimiter byte
+	readfunc  ServerReadFunc
+
+	remoteMap *sync.Map
+
+	//under protected data
+	eDer
+	eDerfunc eDinitfunc
+
+	l          net.Listener
+	cancelfunc context.CancelFunc //cancelfunc for cancel listener(CUT)
+
+	//tempory used for readfunc
+	currentConn net.Conn
+	additional  interface{}
+}
+
+type hConnerServer struct {
+	conn     net.Conn
+	d        byte
+	mu       *sync.Mutex //lock readfunc
+	readfunc ServerReadFunc
+}
+
+func NewServer(
+	ipaddrsocket string,
+	delim byte, readfunc ServerReadFunc, additional interface{}) *Server {
+
+	s := &Server{
+		ipaddr:    ipaddrsocket,
+		readDDL:   0,
+		writeDDL:  0,
+		delimiter: delim,
+		eDer: eDer{
+			eU:     make(chan Errsocket),
+			closed: false,
+			mu:     new(sync.Mutex),
+			pmu:    nil,
+		},
+		remoteMap:  new(sync.Map),
+		readfunc:   readfunc,
+		additional: additional,
+	}
+	s.eDerfunc = errDiversion(&s.eDer)
+	return s
+}
+
+func (t *Server) Listen() error { //Notifies the consumer when an error occurs ASYNCHRONOUSLY
+	if t.readfunc == nil {
+		return errors.New("read function is nil")
+	}
+	listener, err := net.Listen("tcp", t.ipaddr)
+	if err != nil {
+		return err
+	}
+
+	var ctx, cfunc = context.WithCancel(context.Background())
+	t.cancelfunc = cfunc
+	t.l = listener
+	eC := make(chan Errsocket)
+	go t.eDerfunc(eC)
+	go handleListen(t, eC, ctx)
+	return nil
+}
+
+func handleListen(s *Server, eC chan Errsocket, ctx context.Context) {
+	fmt.Println("Start hL")
+	defer fmt.Println("->hL quit")
+	defer close(eC)
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			conn, err := s.l.Accept()
+			if err != nil {
+				s.mu.Lock()
+				eC <- Errsocket{err, s.ipaddr}
+				return
+			}
+			if s.readDDL != 0 {
+				_ = conn.SetReadDeadline(time.Now().Add(s.readDDL))
+			}
+			if s.writeDDL != 0 {
+				_ = conn.SetWriteDeadline(time.Now().Add(s.writeDDL))
+			}
+
+			cctx, childfunc := context.WithCancel(ctx) //TODO: if MAXLIVETIME is needed.
+			//s.remoteMap[conn.RemoteAddr().String()] = childfunc
+			s.remoteMap.Store(conn.RemoteAddr().String(), childfunc)
+
+			ceC := make(chan Errsocket)
+			go s.eDerfunc(ceC)
+			go handleConnServer(&hConnerServer{
+				conn:     conn,
+				d:        s.delimiter,
+				readfunc: s.readfunc,
+				mu:       s.mu,
+			}, ceC, cctx, s)
+		}
+	}
+}
+
+func handleConnServer(h *hConnerServer, eC chan Errsocket, ctx context.Context, s *Server) {
+	fmt.Println("Start hCS:", h.conn.LocalAddr(), "->", h.conn.RemoteAddr())
+	defer fmt.Println("->hCS quit", h.conn.LocalAddr(), "->", h.conn.RemoteAddr())
+	strReqChan := make(chan []byte)
+	defer func() {
+		err := h.conn.Close()
+		<-strReqChan
+		if err != nil {
+			h.mu.Lock()
+			eC <- Errsocket{err, h.conn.RemoteAddr().String()}
+		}
+		close(eC)
+	}()
+
+	go read(&reader{
+		conn:       h.conn,
+		d:          h.d,
+		mu:         h.mu,
+		strReqChan: strReqChan,
+	}, eC)
+
+	for {
+		select {
+		case <-ctx.Done(): //quit manually
+			return
+		case strReq, ok := <-strReqChan: //read a data slice successfully
+			if !ok {
+				return //EOF && d!=0
+			}
+			//h.mu.Lock() //s.mu		why does it need a lock?? Temporary removed it.
+			err := h.readfunc(strReq, &Server{
+				currentConn: h.conn,
+				delimiter:   h.d,
+				remoteMap:   s.remoteMap,
+			}) //requires a lock from hL
+			//h.mu.Unlock()
+			if err != nil {
+				h.mu.Lock()
+				eC <- Errsocket{err, h.conn.RemoteAddr().String()}
+			}
+		}
+	}
+}
+
+/*
+will not wait for the rest of goroutines' error message.
+make sure all connections has exited successfully before doing this
+*/
+func (t *Server) Cut() {
+	t.mu.Lock()
+	err := t.l.Close()
+	if err != nil {
+		t.eU <- Errsocket{err, t.ipaddr}
+	}
+	t.closed = true
+	close(t.eU)
+	t.mu.Unlock()
+	t.cancelfunc()
+}
+
+func (t *Server) CloseRemote(remoteAddr string) {
+	x, ok := t.remoteMap.Load(remoteAddr)
+	if !ok {
+		t.eU <- Errsocket{errors.New(remoteAddr + " does not connected to this server"), t.ipaddr}
+		return
+	}
+	x.(context.CancelFunc)()
+	t.remoteMap.Delete(remoteAddr)
+}
+
+func (t *Server) Close() {
+	remoteAddr := t.currentConn.RemoteAddr().String()
+	x, ok := t.remoteMap.Load(remoteAddr)
+	if !ok {
+		t.eU <- Errsocket{errors.New("internal error"), t.ipaddr}
+		return
+	}
+	x.(context.CancelFunc)()
+	t.remoteMap.Delete(remoteAddr)
+}
+
+func (t *Server) RangeConn() []string {
+	rtnstring := make([]string, 0)
+	t.remoteMap.Range(
+		func(key, value interface{}) bool {
+			rtnstring = append(rtnstring, key.(string))
+			return true
+		})
+	return rtnstring
+}
+
+func (t *Server) SetDeadLine(rDDL time.Duration, wDDL time.Duration) {
+	t.readDDL, t.writeDDL = rDDL, wDDL
+}
+
+func (t *Server) ErrChan() <-chan Errsocket {
+	return t.eU
+}
+
+func (t *Server) GetRemoteAddr() string {
+	if t.currentConn == nil {
+		return ""
+	}
+	return t.currentConn.RemoteAddr().String()
+}
+
+func (t *Server) GetConn() net.Conn {
+	return t.currentConn
+}
+
+func (t *Server) Addon() interface{} {
+	return t.additional
+}
+
+func (t *Server) Write(i interface{}) error {
+	return Write(t.currentConn, i, t.delimiter)
+}
